d3b748740b027c90951df09bca2b91cd22a3fabf
Jonathan Silva <jonathanfelsilva@gmail.com>
2021-10-18 15:11:31 -0300

    Criação da DaoFactory e do SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +15,7 @@ public class Program {
         Department department = new Department(1, "Teste");
         Seller seller = new Seller(1, "Jonathan", "jonathan@email.com", new Date(), 160.00, department);
 
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(department);
         System.out.println(seller);
